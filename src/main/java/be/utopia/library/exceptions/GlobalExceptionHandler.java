437d456260c1777cd35cab84bdf25da0bfff45c0
aaron <aaron.rombouts@gmail.com>
2022-12-14 23:05:19 +0100

    added guards and exception handler

---
+++ src/main/java/be/utopia/library/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,68 @@
+package be.utopia.library.exceptions;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.http.HttpStatus;
+
+import com.google.firebase.auth.FirebaseAuthException;
+
+import java.util.NoSuchElementException;
+
+import javax.persistence.EntityNotFoundException;
+
+@ControllerAdvice
+@ComponentScan
+public class GlobalExceptionHandler {
+
+    Logger logger = LogManager.getLogger(GlobalExceptionHandler.class);
+
+    public void logRequestException(WebRequest request, Level level, Exception ex){
+        String sessionId = request.getSessionId();
+        String exceptionString = ex.toString();
+        logger.log(level, String.format("Global exception handeled for Session [%1$s] with exception [%2$s]", sessionId, exceptionString));
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public final void handle(WebRequest request, Exception ex) {
+        this.logRequestException(request, Level.ERROR, ex);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NoSuchElementException.class)
+     public final void handle(WebRequest request, NoSuchElementException ex){
+          this.logRequestException(request, Level.WARN, ex);
+     }
+
+     @ResponseStatus(HttpStatus.BAD_REQUEST)
+     @ExceptionHandler(IllegalArgumentException.class)
+     public final void handle(WebRequest request, IllegalArgumentException ex){
+          this.logRequestException(request, Level.WARN, ex);
+     }
+
+     @ResponseStatus(HttpStatus.BAD_REQUEST)
+     @ExceptionHandler(EntityNotFoundException.class)
+     public final void handle(WebRequest request, EntityNotFoundException ex){
+          this.logRequestException(request, Level.WARN, ex);
+     }
+
+     @ResponseStatus(HttpStatus.BAD_REQUEST)
+     @ExceptionHandler(NumberFormatException.class)
+     public final void handle(WebRequest request, NumberFormatException ex){
+          this.logRequestException(request, Level.WARN, ex);
+     }
+
+     @ResponseStatus(HttpStatus.UNAUTHORIZED)
+     @ExceptionHandler(FirebaseAuthException.class)
+     public final void handle(WebRequest request, FirebaseAuthException ex){
+          this.logRequestException(request, Level.WARN, ex);
+     }
+
+    
+}
---
+++ src/main/java/be/utopia/library/validation/Guard.java
@@ -0,0 +1,117 @@
+package be.utopia.library.validation;
+
+import java.util.regex.Pattern;
+
+public class Guard {
+
+    private static final String REGEX_NICKNAME = "^([a-zA-Z0-9_.-]){2,30}$";
+    private static final String REGEX_ONLYLETTERS = "^[a-zA-Z]+$";
+    private static final String REGEX_URL = "^(https)://[a-zA-Z0-9-_.]+\\.[a-zA-Z]{2,5}(/.*)?$";
+    private static final String REGEX_TITLE = "^.{2,80}$";
+    private static final String REGEX_TEXTBLOCK = "^.{2,500}$";
+
+
+    /**
+     * Checks if the provided parameter is equal to null
+     */
+    public static<T> T notNull (T t, String caseOfException){
+        if (t == null){
+            throw new IllegalArgumentException(caseOfException + " Exception during notNull Guard: " + t + " is equal to zero");
+        }
+        return t;
+    }
+
+    /**
+     * Checks if the provided string is either null or empty
+     */
+    public static String notBlank (String s, String caseOfException){
+        if (Guard.notNull(s, caseOfException).isBlank())
+            throw new IllegalArgumentException(caseOfException + " Exception during notBlank Guard: " + s + " is blank");
+        return s;
+    }
+
+    /**
+     * Checks if the lenght of the provided string is between a given min (not inclusive) and max bound (not inclusive)
+     */
+    public static String limitSize(String s, int min, int max, String caseOfException){
+        String sNotNull = Guard.notNull(s, caseOfException);
+        if (sNotNull.length() < min || sNotNull.length() > max)
+            throw new IllegalArgumentException(caseOfException + " Exception during limitSize Guard: " + s + " is not between " + min + " and " + max);
+        return s;
+    }
+
+    /**
+     * Checks if the provided string contains only alphabetic characters
+     */
+    public static String onlyLetters(String s, String caseOfException){
+        if(!Pattern.matches(REGEX_ONLYLETTERS, Guard.notNull(s, caseOfException)))
+            throw new IllegalArgumentException(caseOfException + " Exception during onlyLetters Guard: " + s + " contains non alphabetic characters");
+        return s;
+    }
+
+    /**
+     * Checks if the provided string is a valid nickname
+     */
+    public static String validNickname(String s, String caseOfException){
+        if(!Pattern.matches(REGEX_NICKNAME, Guard.notNull(s, caseOfException)))
+            throw new IllegalArgumentException(caseOfException + " Exception during validNickname Guard: " + s + " is not a valid nickname");
+        return s;
+
+    }
+
+    /**
+     * Checks if the provided string is a valid URL
+     */
+    public static String validURL(String s, String caseOfException){
+        if(!Pattern.matches(REGEX_URL, Guard.notNull(s, caseOfException)))
+            throw new IllegalArgumentException(caseOfException + " Exception during validURL Guard: " + s + " is not a valid URL");
+        return s;
+    }
+
+    /**
+     * Checks if the provided string is a valid title
+     */
+
+    public static String validTitle(String s, String caseOfException){
+        if(!Pattern.matches(REGEX_TITLE, Guard.notNull(s, caseOfException)))
+            throw new IllegalArgumentException(caseOfException + " Exception during validTitle Guard: " + s + " is not a valid title");
+        return s;
+    }
+
+    /**
+     * Checks if the provided string is a valid textblock
+     */
+
+    public static String validTextBlock(String s, String caseOfException){
+        if(!Pattern.matches(REGEX_TEXTBLOCK, Guard.notNull(s, caseOfException)))
+            throw new IllegalArgumentException(caseOfException + " Exception during validTextBlock Guard: " + s + " is not a valid textblock");
+        return s;
+    }
+
+    /**
+     * Checks if the provided string is valid according to a custom regex
+     * @param pattern
+     * The regex to use for testing
+     * @param s
+     * The string to test against the custom regex
+     */
+
+    public static String validCustom(String pattern, String s, String caseOfException){
+        if(!Pattern.matches(pattern, Guard.notNull(s, caseOfException)))
+            throw new IllegalArgumentException(caseOfException + " Exception during validCustom Guard: " + s + " is not valid according to the custom regex");
+        return s;
+    }
+
+    /**
+     * Checks if the provided integer lies between a given min (not inclusive) and max (not inclusive)
+     */
+    public static int between (int i, int min, int max, String caseOfException){
+        if ((i < min || i > max))
+            throw new IllegalArgumentException(caseOfException + " Exception during between Guard: " + i + " is not between min " + min + " and max " + max);
+        return i;
+    }
+
+
+
+    
+}
