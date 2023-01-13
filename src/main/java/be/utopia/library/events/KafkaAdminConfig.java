da1ffd8167187914b479688a30424b0978771af7
JohanTorfs <r0831282@student.thomasmore.be>
2023-01-13 20:51:18 +0100

    added kafka template using bean

---
+++ src/main/java/be/utopia/library/events/KafkaAdminConfig.java
@@ -0,0 +1,19 @@
+package be.utopia.library.events;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaAdmin;
+
+@Configuration
+public class KafkaAdminConfig {
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka.broker:9092");
+        return new KafkaAdmin(configs);
+    }
+}
